1f8bfa7fe8001577f5e601504ff2afc2f0106876
santosh sagar <msg2santoshsagar@gmail.com>
2017-11-26 19:10:59 +0530

    Master workspace setup

---
+++ src/main/java/com/alife/flatmates/FlatmatesServer/FlatmatesServerApplication.java
@@ -0,0 +1,12 @@
+package com.alife.flatmates.FlatmatesServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatmatesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatmatesServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alife/flatmates/FlatmatesServer/FlatmatesServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alife.flatmates.FlatmatesServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlatmatesServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
